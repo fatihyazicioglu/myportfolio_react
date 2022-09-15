a769197fd059fb1bdf58e3a714ec7b7be58b9829
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-09-16 00:45:13 +0200

    updated

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,14 +1,33 @@
 import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
-function Topbar() {
+function Topbar({menuOpen, setMenuOpen}) {
   return (
-    <div className="topbar" id="topbar">
+    <div className={"topbar " + (menuOpen && "active")}  id="topbar">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">asadas</a>
+          <a href="#intro" className="logo">
+            F.Y.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+49 163 517 42 84</span>
           </div>
-          <h1>Hello</h1>
-        <div className="right">RIGHT</div>
+
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>fy134388@gmail.com</span>
+          </div>
+        </div>
+        <h1>Hello</h1>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+
+          </div>
+        </div>
       </div>
     </div>
   );
