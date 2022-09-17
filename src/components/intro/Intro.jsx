f0472fe448a14f6cd64f9c6711b37f284107922e
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-09-17 14:44:37 +0200

    updated

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,29 @@
-import "./intro.scss"
-
+import "./intro.scss";
+import { ExpandMore } from "@material-ui/icons";
 
 function Intro() {
   return (
-    <div className='intro' id='intro'>Intro</div>
-  )
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="/man.jpg" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I"m</h2>
+          <h1>Fatih Yazicioglu</h1>
+          <h3>
+            Frontend Developer <span></span>
+          </h3>
+          <ExpandMore className="downIcon" />
+        </div>
+
+        {/* <a href="#portfolio">
+        <img src="/downarrow.webp" alt="" /></a> */}
+      </div>
+    </div>
+  );
 }
 
-export default Intro
+export default Intro;
